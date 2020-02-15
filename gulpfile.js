b5452bae435d52485c4ef81b928ce1a4875b37bc
Mike Dreyfus <mdreyfus@twitch.tv>
2020-02-15 14:52:03 -0800

    upgraded gulp and dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var uglify = require('gulp-uglify');
+var uglify = require('gulp-terser');
 var rename = require('gulp-rename');
 
 // compile scss to css
